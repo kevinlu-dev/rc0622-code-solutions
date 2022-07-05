1f867dedb171cc09a7fcf60d2dbc57195c1f74da
Kevin Lu <kevinlu.dev@gmail.com>
2022-07-05 19:30:42 +0000

    completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,51 @@
+var width = 3;
+
+var height = 3;
+
+var area = width * height;
+console.log('value of area:', area);
+console.log('value of typeof area:', typeof area);
+
+var bill = 6;
+
+var payment = 9;
+
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('value of typeof change:', typeof change);
+
+var quizzes = 70;
+
+var midterm = 80;
+
+var final = 90;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('value of typeof grade:', typeof grade);
+
+var firstName = 'Kevin';
+
+var lastName = 'Lu';
+
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('value of typeof fullName', typeof fullName);
+
+var ph = 3;
+
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('value of typeof isAcidic:', typeof isAcidic);
+
+var headCount = 300;
+
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('value of typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('value of typeof motto:', typeof motto);
