7c55ae0243addeab00df1a33b032e8f360d453fe
Kevin Lu <kevinlu.dev@gmail.com>
2022-07-13 19:12:56 +0000

    completed javascript-forms exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,13 @@
+var contactQuery = document.getElementById('contact-form');
+
+contactQuery.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formAll = {
+    name: contactQuery.elements.name.value,
+    email: contactQuery.elements.email.value,
+    message: contactQuery.elements.message.value
+  };
+  console.log(formAll);
+});
+
+document.getElementById('contact-form').reset();
